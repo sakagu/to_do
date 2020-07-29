d1b4ee87e71f0adc59dae5eaf25c79b99319b10b
sakagu <sakatoshi10416@gmail.com>
2020-07-29 19:09:02 +0900

    add

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Folder;
 use App\Task;
 use App\Http\Requests\CreateTask;
+use App\Http\Requests\EditTask;
 use Illuminate\Http\Request;
 
 class TaskController extends Controller
@@ -28,7 +29,7 @@ class TaskController extends Controller
         'folder_id' => $id
     ]);
     }
-    
+
     public function create(int $id, CreateTask $request)
     {
     $current_folder = Folder::find($id);
@@ -42,6 +43,32 @@ class TaskController extends Controller
     return redirect()->route('tasks.index', [
         'id' => $current_folder->id,
         ]);
+    }
+
+    public function showEditForm(int $id, int $task_id)
+    {
+    $task = Task::find($task_id);
+
+    return view('tasks/edit', [
+        'task' => $task,
+    ]);
+    }
+
+    public function edit(int $id, int $task_id, EditTask $request)
+    {
+    // 1
+    $task = Task::find($task_id);
+
+    // 2
+    $task->title = $request->title;
+    $task->status = $request->status;
+    $task->due_date = $request->due_date;
+    $task->save();
+
+    // 3
+    return redirect()->route('tasks.index', [
+        'id' => $task->folder_id,
+    ]);
     }
 
 }
